a7cb6cfc385eb7a5fd9c7f6efb4a2cd42855ce6d
Cesar Fuentes <cesarfuentes@Cesars-MacBook-Pro.local>
2022-07-04 13:35:53 -0500

    Added hover text for navbar

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ class MyNavbar extends HTMLElement {
         <div class="navbar">
             <div class="buttons">
             <span data-text="Home" class="tooltip">    <form action="/index.html"><input type="image" class="btn" src="icons/Furniture_NH_Inv_Icon.png" /></form>   </span> 
-            <form action="/education.html"><input type="image" class="btn" src="icons/Pocketbook_NH_Inv_Icon.png" /></form>
-            <form action="/work.html"><input type="image" class="btn" src="icons/Construction_Helmet_NH_Inv_Icon.png" /></form>
-            <form action="/projects.html"><input type="image" class="btn" src="icons/Dumbbell_NH_Inv_Icon.png" /></form>
+            <span data-text="Education" class="tooltip"> <form action="/education.html"><input type="image" class="btn" src="icons/Pocketbook_NH_Inv_Icon.png" /></form>  </span> 
+            <span data-text="Work Exp." class="tooltip"> <form action="/work.html"><input type="image" class="btn" src="icons/Construction_Helmet_NH_Inv_Icon.png" /></form>  </span> 
+            <span data-text="Projects" class="tooltip"> <form action="/projects.html"><input type="image" class="btn" src="icons/Dumbbell_NH_Inv_Icon.png" /></form>  </span> 
             <br>
-            <form action="/leader.html"><input type="image" class="btn" src="icons/Pennant_NH_Inv_Icon.png" /></form>
-            <form action="/skills.html"><input type="image" class="btn" src="icons/Slingshot_NH_Inv_Icon.png" />  </form>
-            <form action="/contact.html"><input type="image" class="btn" src="icons/Mailbox_NH_Inv_Icon.png" /></form>
-            <form action="/about.html"><input type="image" class="btn" src="icons/Accessory_NH_Inv_Icon.png" /></form>
+            <span data-text="Leadership" class="tooltip"> <form action="/leader.html"><input type="image" class="btn" src="icons/Pennant_NH_Inv_Icon.png" /></form>  </span> 
+            <span data-text="Skills" class="tooltip"> <form action="/skills.html"><input type="image" class="btn" src="icons/Slingshot_NH_Inv_Icon.png" />  </form>  </span> 
+            <span data-text="Contact" class="tooltip"> <form action="/contact.html"><input type="image" class="btn" src="icons/Mailbox_NH_Inv_Icon.png" /></form> </span> 
+            <span data-text="About" class="tooltip"> <form action="/about.html"><input type="image" class="btn" src="icons/Accessory_NH_Inv_Icon.png" /></form>  </span> 
             </div>
         </div>
         `
