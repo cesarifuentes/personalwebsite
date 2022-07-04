f8b379d7fc20e1dc496482a0377b6dd0809ff7e6
Cesar Fuentes <cesarfuentes@Cesars-MacBook-Pro.local>
2022-07-03 22:42:13 -0500

    Added right text panel

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function loadElements() {
+    // var slides = document.getElementsByClassName("nav-placeholder");
+    // for (var i = 0; i < slides.length; i++) {
+    //     slides.item(i).innerHTML = "";
+    // }
+
+    $("#nav-placeholder").load("nav.html");
+
+}
