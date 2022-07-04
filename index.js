57bc177ed609efc70563ee143b5ef3967103eb73
Cesar Fuentes <cesarfuentes@Cesars-MacBook-Pro.local>
2022-07-04 00:39:47 -0500

    Added animation

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-
-function loadElements() {
-    // var slides = document.getElementsByClassName("nav-placeholder");
-    // for (var i = 0; i < slides.length; i++) {
-    //     slides.item(i).innerHTML = "";
-    // }
-
-    $("#nav-placeholder").load("nav.html");
-
+class MyNavbar extends HTMLElement {
+    // called every time this element is connected to a document
+    connectedCallback() {
+        this.innerHTML = `
+        <div class="navbar">
+            <div class="buttons">
+            <form action="/index.html"><input type="image" class="btn" src="icons/Furniture_NH_Inv_Icon.png" /></form>
+            <form action="/education.html"><input type="image" class="btn" src="icons/Pocketbook_NH_Inv_Icon.png" /></form>
+            <form action="/work.html"><input type="image" class="btn" src="icons/Construction_Helmet_NH_Inv_Icon.png" /></form>
+            <form action="/projects.html"><input type="image" class="btn" src="icons/Dumbbell_NH_Inv_Icon.png" /></form>
+            <br>
+            <form action="/leader.html"><input type="image" class="btn" src="icons/Pennant_NH_Inv_Icon.png" /></form>
+            <form action="/skills.html"><input type="image" class="btn" src="icons/Slingshot_NH_Inv_Icon.png" />  </form>
+            <form action="/contact.html"><input type="image" class="btn" src="icons/Mailbox_NH_Inv_Icon.png" /></form>
+            <form action="/about.html"><input type="image" class="btn" src="icons/Accessory_NH_Inv_Icon.png" /></form>
+            </div>
+        </div>
+        `
+    }
 }
+customElements.define('navbar-template', MyNavbar)
+
